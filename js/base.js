d34457c52037e2d0120586a502f4486361abc9e0
RosaEugenia <rosa.eugenia.ortega@gmail.com>
2021-05-04 14:38:36 -0500

    modificacion del archivo link

--- js/base.js
+++ js/base.js
@@ -1,32 +1,33 @@
 console.log('Hola Mundo')
-/*
-	Ejercicio 1:
-	Pedir 2 variables por prompt
-	Si a > b dividir a entre b
-	Si a < b dividir sumar a más b
-	si a == b multiplicar ambos numeros
-	Imprimir el resultado en consola
-*/
-/***
-	Pedir por el promt un numero, y comprobar si es par o impar
-*/
+    /*
+    	Ejercicio 1:
+    	Pedir 2 variables por prompt
+    	Si a > b dividir a entre b
+    	Si a < b dividir sumar a más b
+    	si a == b multiplicar ambos numeros
+    	Imprimir el resultado en consola
+    */
+    /***
+    	Pedir por el promt un numero, y comprobar si es par o impar
+    */
 
 const numberOne = parseInt(window.prompt("Cual es la primer variable a"))
-console.log( typeof numberOne)
-const numberTwo = parseInt( window.prompt("Cual es la primer variable b"))
-let result = 
-if (numberOne > numberTwo){
-result = NumerOne / numberTwo
-}else if( numberOne < numberTwo){
-  result = numberOne + numberTwo
-}else{
-  result= numberOne * numerTwo
+console.log(typeof numberOne)
+const numberTwo = parseInt(window.prompt("Cual es la primer variable b"))
+let result
+if (numberOne > numberTwo) {
+    result = NumerOne / numberTwo
+} else
+if (numberOne < numberTwo) {
+    result = numberOne + numberTwo
+} else {
+    result = numberOne * numerTwo
 }
 
-const parOImpar = parseInt( window.prompt("Dame un numero"))
+const parOImpar = parseInt(window.prompt("Dame un numero"))
 console.log(parOImpar)
-if(parOImpar % 2 === 0){
-  console.log(`el nmero ${parOImpar} es par`)
-}else{
-  console.log(`el numero ${parOImpar} es impar`)
+if (parOImpar % 2 === 0) {
+    console.log(`el numero ${parOImpar} es par`)
+} else {
+    console.log(`el numero ${parOImpar} es impar`)
 }
